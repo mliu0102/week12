b8c67ab8c5a827aba7b46265ea2b13182e2a3e18
mliu0102 <mliu0102@student.monash.edu>
2024-10-18 23:11:02 +1100

    Add GitHub Pages deployment setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,6 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/week12/', // Add this line
 })
-
