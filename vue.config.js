4b4153868057aaf3b113e156f0d8519aba89f464
mliu0102 <mliu0102@student.monash.edu>
2024-10-18 22:54:14 +1100

    Add GitHub Pages deployment setup

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/week12/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/week12/'  // Replace with your repository's name
+    : '/'
+}
